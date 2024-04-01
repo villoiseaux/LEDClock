6ad1e0c8c5219e05d9ba296b9d8121ae485db424
jpinon <github@jean.pinon.name>
2024-04-01 18:49:52 +0200

    add messages.h

--- software/ESP_Clock/messages.h
+++ software/ESP_Clock/messages.h
@@ -5,8 +5,21 @@
 #define MSG_GET_TIME_FROM_NTP "Time"
 #define MSG_GET_LOCALES "Geoloc"
 #define MSG_REBOOT "reboot"
-#define MSG_WORNING_CONNECTIONS "Warning"
+#define MSG_WARNING_CONNECTIONS "Warning"
 #define MSG_CHECK_FW_UPDATE "New Fw ?"
 #define MSG_UPDATE_FAILED "failed"
 #define MSG_UPDATE_NOTHING "Nothing"
 #define MSG_UPDATE_DONE "Done"
+
+#ifdef MSG_IN_FRENCH
+  #define MSG_SEARCH_WIFI "WiFi"
+  #define MSG_CONFIG_MODE "Config"
+  #define MSG_GET_TIME_FROM_NTP "Heure"
+  #define MSG_GET_LOCALES "Geoloc"
+  #define MSG_REBOOT "reboot"
+  #define MSG_WARNING_CONNECTIONS "Danger"
+  #define MSG_CHECK_FW_UPDATE "MAJ ?"
+  #define MSG_UPDATE_FAILED "Erreur"
+  #define MSG_UPDATE_NOTHING "Rien"
+  #define MSG_UPDATE_DONE "Fait"
+#endif
